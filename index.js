24ab38c76ef656a66576971ec5fa4b4e2a450da4
David <dvddcrz@gmail.com>
2024-06-13 16:35:43 +0100

    changed db structure

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { PrismaClient } = require('@prisma/client');
 
 dotenv.config();
-const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 const corsOptions = {
-    origin: "*"
-}
+	origin: '*',
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Something went wrong!' });
+	console.error(err.stack);
+	res.status(500).json({message: 'Something went wrong!'});
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
