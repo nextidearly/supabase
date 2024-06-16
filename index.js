9b3677b497e803acfd19a957096eae3482cfb2dc
David <dvddcrz@gmail.com>
2024-06-16 12:47:23 +0100

    add first route

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.use(express.json());
 const liveCampaignsRouter = require('./routes/liveCampaigns');
 const userCreatedCampaignsRouter = require('./routes/userCreatedCampaigns');
 
+// Example: /v1/campaigns/live?page=0&pageSize=10
 app.use('/v1', liveCampaignsRouter);
+
+// Example: /v1/user/:id/campaigns?page=0&pageSize=10
 app.use('/v1', userCreatedCampaignsRouter);
 
 // Start the server
