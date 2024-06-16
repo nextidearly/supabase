9cb57607cfcc9191142e386335099425280003cf
David <dvddcrz@gmail.com>
2024-06-17 00:44:45 +0100

    add tests

--- tests/getLiveStatus.test.js
+++ tests/getLiveStatus.test.js
@@ -31,6 +31,7 @@ beforeAll(() => {
 
 afterAll(() => {
   const MockDate = require('mockdate');
+
   MockDate.reset(); // Reset the mocked date
 });
 
@@ -70,17 +71,13 @@ describe('getLiveStatus', () => {
   });
 
   test('Campaign starts now and is unpublished', () => {
-    const spoofedCampaign = createSpoofedCampaign(0, 30 * 24 * 60, false);
+    const spoofedCampaign = createSpoofedCampaign(-1, 30 * 24 * 60, false);
     const status = getLiveStatus(spoofedCampaign);
 
     expect(status.isLive).toBe(false);
     expect(status.isFinished).toBe(false);
     expect(status.timeUntilEnd).toBeNull();
-    expect(status.timeSinceStart).toEqual({
-      days: 0,
-      hours: 0,
-      minutes: 0,
-    });
+    expect(status.timeSinceStart).toBeNull();
     expect(status.timeUntilStart).toBeNull();
   });
 
@@ -95,11 +92,7 @@ describe('getLiveStatus', () => {
     expect(status.isLive).toBe(false);
     expect(status.isFinished).toBe(true);
     expect(status.timeUntilEnd).toBeNull();
-    expect(status.timeSinceStart).toEqual({
-      days: 31,
-      hours: 0,
-      minutes: 0,
-    });
+    expect(status.timeSinceStart).toBeNull();
     expect(status.timeUntilStart).toBeNull();
   });
 
@@ -133,6 +126,146 @@ describe('getLiveStatus', () => {
       days: 0,
       hours: 0,
       minutes: 1,
+    });
+    expect(status.timeUntilStart).toBeNull();
+  });
+
+  test('Campaign ends exactly now', () => {
+    const spoofedCampaign = createSpoofedCampaign(
+      -30 * 24 * 60,
+      30 * 24 * 60,
+      true,
+    );
+    const status = getLiveStatus(spoofedCampaign);
+
+    expect(status.isLive).toBe(false);
+    expect(status.isFinished).toBe(true);
+    expect(status.timeUntilEnd).toBeNull();
+    expect(status.timeSinceStart).toBeNull();
+    expect(status.timeUntilStart).toBeNull();
+  });
+
+  test('Campaign started and ended in the past', () => {
+    const spoofedCampaign = createSpoofedCampaign(
+      -60 * 24 * 60,
+      30 * 24 * 60,
+      true,
+    );
+    const status = getLiveStatus(spoofedCampaign);
+
+    expect(status.isLive).toBe(false);
+    expect(status.isFinished).toBe(true);
+    expect(status.timeUntilEnd).toBeNull();
+    expect(status.timeSinceStart).toBeNull();
+    expect(status.timeUntilStart).toBeNull();
+  });
+
+  test('Campaign starts in 1 hour', () => {
+    const spoofedCampaign = createSpoofedCampaign(60, 30 * 24 * 60, true);
+    const status = getLiveStatus(spoofedCampaign);
+
+    expect(status.isLive).toBe(false);
+    expect(status.isFinished).toBe(false);
+    expect(status.timeUntilEnd).toBeNull();
+    expect(status.timeSinceStart).toBeNull();
+    expect(status.timeUntilStart).toEqual({
+      days: 0,
+      hours: 1,
+      minutes: 0,
+    });
+  });
+
+  test('Campaign starts in 30 days', () => {
+    const spoofedCampaign = createSpoofedCampaign(
+      30 * 24 * 60,
+      30 * 24 * 60,
+      true,
+    );
+    const status = getLiveStatus(spoofedCampaign);
+
+    expect(status.isLive).toBe(false);
+    expect(status.isFinished).toBe(false);
+    expect(status.timeUntilEnd).toBeNull();
+    expect(status.timeSinceStart).toBeNull();
+    expect(status.timeUntilStart).toEqual({
+      days: 30,
+      hours: 0,
+      minutes: 0,
+    });
+  });
+
+  test('Unpublished campaign in the past', () => {
+    const spoofedCampaign = createSpoofedCampaign(
+      -30 * 24 * 60,
+      30 * 24 * 60,
+      false,
+    );
+    const status = getLiveStatus(spoofedCampaign);
+
+    expect(status.isLive).toBe(false);
+    expect(status.isFinished).toBe(false);
+    expect(status.timeUntilEnd).toBeNull();
+    expect(status.timeSinceStart).toBeNull();
+    expect(status.timeUntilStart).toBeNull();
+  });
+
+  test('Unpublished campaign in the future', () => {
+    const spoofedCampaign = createSpoofedCampaign(
+      30 * 24 * 60,
+      30 * 24 * 60,
+      false,
+    );
+    const status = getLiveStatus(spoofedCampaign);
+
+    expect(status.isLive).toBe(false);
+    expect(status.isFinished).toBe(false);
+    expect(status.timeUntilEnd).toBeNull();
+    expect(status.timeSinceStart).toBeNull();
+    expect(status.timeUntilStart).toEqual({
+      days: 30,
+      hours: 0,
+      minutes: 0,
+    });
+  });
+
+  test('Campaign started exactly now', () => {
+    const spoofedCampaign = createSpoofedCampaign(0, 30 * 24 * 60, true);
+    const status = getLiveStatus(spoofedCampaign);
+
+    expect(status.isLive).toBe(true);
+    expect(status.isFinished).toBe(false);
+    expect(status.timeUntilEnd).toEqual({
+      days: 30,
+      hours: 0,
+      minutes: 0,
+    });
+    expect(status.timeSinceStart).toEqual({
+      days: 0,
+      hours: 0,
+      minutes: 0,
+    });
+    expect(status.timeUntilStart).toBeNull();
+  });
+
+  test('Campaign ends exactly in 1 hour', () => {
+    const spoofedCampaign = createSpoofedCampaign(
+      -30 * 24 * 60 + 60, // Start 29 days, 23 hours, and 1 minute ago
+      30 * 24 * 60, // Duration 30 days
+      true,
+    );
+    const status = getLiveStatus(spoofedCampaign);
+
+    expect(status.isLive).toBe(true);
+    expect(status.isFinished).toBe(false);
+    expect(status.timeUntilEnd).toEqual({
+      days: 0,
+      hours: 1,
+      minutes: 0,
+    });
+    expect(status.timeSinceStart).toEqual({
+      days: 29,
+      hours: 23,
+      minutes: 0,
     });
     expect(status.timeUntilStart).toBeNull();
   });
--- utils/getLiveStatus.js
+++ utils/getLiveStatus.js
@@ -1,36 +1,36 @@
 /**
- * Determines if a campaign is live, calculates the number of days until the campaign ends,
- * calculates the number of days since the campaign started, and calculates the number of days
- * until the campaign starts if it is in the future.
- *
- * @param {Object} campaign - The campaign object.
- * @param {string} campaign.start_date - The start date of the campaign.
- * @param {string} campaign.end_date - The end date of the campaign.
- * @param {boolean} campaign.published - The published status of the campaign.
- * @returns {Object} An object containing the live status, days until the campaign ends, days since the campaign started,
- * and days until the campaign starts if it is in the future.
- * @returns {boolean} isLive - Whether the campaign is live.
- * @returns {number|null} daysUntilEnd - The number of days until the campaign ends, or null if not live.
- * @returns {number|null} daysSinceStart - The number of days since the campaign started, or null if start date is in future.
- * @returns {boolean} startsInFuture - Whether the campaign's start date is in the future.
- * @returns {boolean} isFinished - Whether the campaign has finished.
- * @returns {Object|null} timeUntilEnd - The time until the campaign ends, or null if not live.
- * @returns {Object|null} timeSinceStart - The time since the campaign started, or null if start date is in future.
- * @returns {Object|null} timeUntilStart - The time until the campaign starts, or null if it has already started.
- */
+
+Determines if a campaign is live, calculates the number of days until the campaign ends,
+calculates the number of days since the campaign started, and calculates the number of days
+until the campaign starts if it is in the future.
+@param {Object} campaign - The campaign object.
+@param {string} campaign.start_date - The start date of the campaign.
+@param {string} campaign.end_date - The end date of the campaign.
+@param {boolean} campaign.published - The published status of the campaign.
+@returns {Object} An object containing the live status, days until the campaign ends, days since the campaign started,
+and days until the campaign starts if it is in the future.
+@returns {boolean} isLive - Whether the campaign is live.
+@returns {number|null} daysUntilEnd - The number of days until the campaign ends, or null if not live.
+@returns {number|null} daysSinceStart - The number of days since the campaign started, or null if start date is in future.
+@returns {boolean} startsInFuture - Whether the campaign's start date is in the future.
+@returns {boolean} isFinished - Whether the campaign has finished.
+@returns {Object|null} timeUntilEnd - The time until the campaign ends, or null if not live.
+@returns {Object|null} timeSinceStart - The time since the campaign started, or null if start date is in future or unpublished.
+@returns {Object|null} timeUntilStart - The time until the campaign starts, or null if it has already started.
+*/
 const getLiveStatus = (campaign) => {
   const now = new Date();
   const startDate = new Date(campaign.start_date);
   const endDate = new Date(campaign.end_date);
-
   const startsInFuture = now < startDate;
-  const isLive = campaign.published && now >= startDate && now <= endDate;
-  const isFinished = now > endDate;
+  const isLive = campaign.published && now >= startDate && now < endDate;
+  const isFinished = campaign.published && now >= endDate;
 
   const timeUntilEnd = isLive ? getTimeDifference(now, endDate) : null;
-  const timeSinceStart = !startsInFuture
-    ? getTimeDifference(startDate, now)
-    : null;
+  const timeSinceStart =
+    !startsInFuture && !isFinished && campaign.published
+      ? getTimeDifference(startDate, now)
+      : null;
   const timeUntilStart = startsInFuture
     ? getTimeDifference(now, startDate)
     : null;
@@ -46,15 +46,15 @@ const getLiveStatus = (campaign) => {
 };
 
 /**
- * Calculates the time difference between two dates and returns the difference in days, hours, and minutes.
- *
- * @param {Date} start - The start date.
- * @param {Date} end - The end date.
- * @returns {Object} An object containing the difference in days, hours, and minutes.
- * @returns {number} days - The number of full days between the two dates.
- * @returns {number} hours - The number of hours remaining after accounting for full days.
- * @returns {number} minutes - The number of minutes remaining after accounting for full hours.
- */
+  
+  Calculates the time difference between two dates and returns the difference in days, hours, and minutes.
+  @param {Date} start - The start date.
+  @param {Date} end - The end date.
+  @returns {Object} An object containing the difference in days, hours, and minutes.
+  @returns {number} days - The number of full days between the two dates.
+  @returns {number} hours - The number of hours remaining after accounting for full days.
+  @returns {number} minutes - The number of minutes remaining after accounting for full hours.
+  */
 const getTimeDifference = (start, end) => {
   const diffMs = end - start;
   const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
