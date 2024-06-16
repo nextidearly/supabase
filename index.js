b71da5bfcd414e49bfb83731a5296a2d6646f1a9
David <dvddcrz@gmail.com>
2024-06-16 12:37:01 +0100

    add first route

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const getRandomElements = (array, count) => {
 
 // [DASHBOARD] - Latest live campaigns component
 // Endpoint to get latest live campaigns with pagination and additional data
-// Example: /api/campaigns/live?page=0&pageSize=10
+// Example: /api/v1/campaigns/live?page=0&pageSize=10
 router.get('/campaigns/live', async (req, res) => {
   const { page, pageSize } = req.query;
   const { limit, offset } = getPagination(page, pageSize);
@@ -68,6 +68,65 @@ router.get('/campaigns/live', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Live campaigns not found' });
+  }
+});
+
+// [DASHBOARD] - Campaigns you created component
+// Endpoint to get campaigns created by a user with pagination, user names, avatars, and computed scores per user
+// Example: /api/v1//user/:id/campaigns?page=0&pageSize=10
+router.get('/user/:id/campaigns', async (req, res) => {
+  const userId = req.params.id;
+  const { page, pageSize } = req.query;
+  const { limit, offset } = getPagination(page, pageSize);
+
+  try {
+    const campaigns = await prisma.campaigns.findMany({
+      where: { admin_id: userId },
+      skip: offset,
+      take: limit,
+      include: {
+        users: {
+          include: {
+            user: {
+              select: {
+                id: true,
+                user_name: true,
+                avatar_url: true,
+              },
+            },
+          },
+        },
+        user_scores: true,
+      },
+    });
+
+    const campaignsWithUserDetails = campaigns.map((campaign) => {
+      const usersWithScores = campaign.users.map((campaignUser) => {
+        const userScore = campaign.user_scores
+          .filter((score) => score.user_id === campaignUser.user.id)
+          .reduce((acc, score) => acc + score.score, 0);
+
+        return {
+          userName: campaignUser.user.user_name,
+          avatarUrl: campaignUser.user.avatar_url,
+          totalUserScore: userScore,
+          blacklisted: campaignUser.blacklisted, // Include blacklisted status
+        };
+      });
+
+      return {
+        id: campaign.id,
+        name: campaign.name,
+        start_date: campaign.start_date,
+        end_date: campaign.end_date,
+        users: usersWithScores,
+      };
+    });
+
+    res.json(campaignsWithUserDetails);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Campaigns not found' });
   }
 });
 
