74f9333c05832780b3ab3c914af4c7845b41eeff
nextidearly <talenteddev0125@gmail.com>
2024-06-17 09:56:24 +0100

    [removed] tests

--- prisma/migrations/20240614232330_added_test/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Campaign" ALTER COLUMN "created_at" SET DEFAULT now();
-
--- AlterTable
-ALTER TABLE "Tweet" ALTER COLUMN "created_at" SET DEFAULT now();
-
--- AlterTable
-ALTER TABLE "UserScore" ALTER COLUMN "created_at" SET DEFAULT now();
